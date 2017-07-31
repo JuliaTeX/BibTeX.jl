e572db8472ead979672d296da171f0f1ba94607e
autodocs <autodocs>
2017-07-31 05:05:55 +0000

    build based on 19b3a88

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "BibTeX.parse_bibtex",
     "category": "Method",
-    "text": "parse_bibtex(text)\n\nThis is a simple, input parser for BibTex. I had trouble finding a standard specification, but I've included several features of real BibTex.\n\njulia> using BibTeX\n\njulia> result = parse_bibtex(\"\"\"\n            @comment blah blah\n            @string{short = long}\n            @a{b,\n              c = {c {c}}, % blah blah\n              d = \"d d\",\n              e = f # short\n            }\n            \"\"\");\n\njulia> result[\"b\"][\"type\"]\n\"a\"\n\njulia> result[\"b\"][\"c\"]\n\"c c\"\n\njulia> result[\"b\"][\"d\"]\n\"d d\"\n\njulia> result[\"b\"][\"e\"]\n\"f short\"\n\njulia> parse_bibtex(\"@book\")\nERROR: Expected { on line 1\n[...]\n\njulia> parse_bibtex(\"@book@\")\nERROR: Expected { on line 1\n[...]\n\n\n\n"
+    "text": "parse_bibtex(text)\n\nThis is a simple input parser for BibTex. I had trouble finding a standard specification, but I've included several features of real BibTex.\n\njulia> using BibTeX\n\njulia> result = parse_bibtex(\"\"\"\n            @string{short = long}\n            @a{b,\n              c = { {c} c},\n              d = \"d d\",\n              e = f # short\n            }\n            \"\"\");\n\njulia> result[\"b\"][\"type\"]\n\"a\"\n\njulia> result[\"b\"][\"c\"]\n\"{ c } c\"\n\njulia> result[\"b\"][\"d\"]\n\"d d\"\n\njulia> result[\"b\"][\"e\"]\n\"f short\"\n\njulia> parse_bibtex(\"@book\")\nERROR: Expected { on line 1\n[...]\n\njulia> parse_bibtex(\"@book@\")\nERROR: Expected { on line 1\n[...]\n\n\n\n"
 },
 
 {
